dd729cdc8f12f90db32e585dfb9461bcc08090e2
koma0524 <167531732+koma0524@users.noreply.github.com>
2024-04-29 00:21:55 +0900

    GetPokeStatus.java(statusコマンドに日本語名、英語名どちらを入力しても動作)
    statusコマンドの後ろのキャラクター名入力を日本語名、英語名どちらでしても動作するように変更しています

--- src/main/java/cli/commands/status/GetPokeStatus.java
+++ src/main/java/cli/commands/status/GetPokeStatus.java
@@ -2,6 +2,10 @@ package cli.commands.status;
 
 import cli.utils.HttpRequest;
 import cli.utils.Logger;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GetPokeStatus implements Runnable {
   private String name;
@@ -12,16 +16,50 @@ public class GetPokeStatus implements Runnable {
 
   @Override
   public void run() {
+    String ename = "";
+
+    if(!isEnglish(name)){
+      try {
+        // locales.jsonファイルの内容を読み込む
+        String content = new String(Files.readAllBytes(Paths.get("src/main/resources/locales.json")));
+        JSONArray jsonArray = new JSONArray(content);
+        
+        // 配列をループして、指定された日本語の名前に対応する英語の名前を探す
+        for (int i = 0; i < jsonArray.length(); i++) {
+          JSONObject jsonObject = jsonArray.getJSONObject(i);
+          
+          if(jsonObject.getString("ja").equals(name)) {
+            // 名前が一致した場合、英語の名前を取得
+            ename = jsonObject.getString("en").toLowerCase();;
+            break;
+          }
+        }
+        
+        if(ename.isEmpty()) {
+          System.out.println("指定された名前に対応するポケモンが見つかりません: " + name);
+          return;
+        }
+        
+      } catch(Exception e) {
+        System.out.println("エラーが発生しました: " + e.getMessage());
+        return;
+      }
+    }else{
+      ename = name;
+    }
+
+    System.out.println("英語名:" + ename);
+
     // ポケモンのデータを取得する
     // 参照: https://pokeapi.co/docs/v2#pokemon
-    HttpRequest fetcher = new HttpRequest("https://pokeapi.co/api/v2/pokemon/" + name);
+    HttpRequest fetcher = new HttpRequest("https://pokeapi.co/api/v2/pokemon/" + ename);
     String res = fetcher.getResponse();
 
     // resからstatsを抜き出す
     String[] stats = res.split("\"stats\":\\[")[1].split("\\]")[0].split("\\},\\{");
 
     // 種族値を表示させる
-    Logger.attention("Pokemon status for " + name + ":");
+    Logger.attention(name + "の種族値" + ":");
     System.out.println();
     for (String stat: stats) {
       String statName = stat.split("\"name\":\"")[1].split("\"")[0];
@@ -30,5 +68,10 @@ public class GetPokeStatus implements Runnable {
       Logger.success(Integer.toString(baseStat));
       System.out.println();
     }
+  }
+
+  // 英語かどうかを判断するメソッド
+  private boolean isEnglish(String str) {
+    return str.matches("[a-zA-Z]+");
   }
 }
